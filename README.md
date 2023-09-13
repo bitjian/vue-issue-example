8cffcf50f5413995a00d16e04dda0063e2617e21
bitjian <1183048003@qq.com>
2023-09-13 15:03:46 +0800

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# vue-issue-example
+## 安装
+
+```
+npm install
+```
+
+## 客户端启动
+
+npm run serve
+
+## 服务端启动
+
+```js
+  npm run build
+  npm run build:ssr
+  npm run serve:ssr
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <div
+            class="m-image-padding"
+            v-if="aspectRatio"
+            :style="{ backgroundColor: 'red' }"
+        />
+        <h2>2222222222</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            aspectRatio: typeof window === 'undefined' ? undefined : 2,
+        };
+    },
+    mounted() {
+        console.log(this.aspectRatio)
+    }
+};
+</script>
